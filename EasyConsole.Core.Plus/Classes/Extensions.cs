db48346646ff48ba9348b266af120f629b55f49a
Marco <emme.milani@hotmail.it>
2019-09-30 16:51:44 +0200

    remvoved string extension. add non blocking console prompt

--- EasyConsole.Core.Plus/Classes/Extensions.cs
+++ EasyConsole.Core.Plus/Classes/Extensions.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace EasyConsole.Core.Plus.Classes
 {
@@ -8,27 +10,51 @@ namespace EasyConsole.Core.Plus.Classes
         {
             Output.WriteLine(text);
         }
-        
+
         public static void Prompt(this string text, ConsoleColor color)
         {
-            Output.WriteLine(color,text);
+            Output.WriteLine(color, text);
         }
 
         public static void PromptWarning(this string text)
         {
-            Output.WriteLine(ConsoleColor.Yellow,text);
-        }
-        
-        public static void PromptError(this string text)
-        {
-            Output.WriteLine(ConsoleColor.Red,text);
-        }
-        
-        public static void PromptSuccess(this string text)
-        {
-            Output.WriteLine(ConsoleColor.Green,text);
+            Output.WriteLine(ConsoleColor.Yellow, text);
         }
 
-       
+        public static void PromptError(this string text)
+        {
+            Output.WriteLine(ConsoleColor.Red, text);
+        }
+
+        public static void PromptSuccess(this string text)
+        {
+            Output.WriteLine(ConsoleColor.Green, text);
+        }
+
+
+        public static void NonBlockingPrompt(this string text)
+        {
+            Task.Run(text.Prompt);
+        }
+
+        public static void NonBlockingPrompt(this string text, ConsoleColor color)
+        {
+            Task.Run(() => text.Prompt(color));
+        }
+
+        public static void NonBlockingPromptWarning(this string text)
+        {
+            Task.Run(text.PromptWarning);
+        }
+
+        public static void NonBlockingPromptError(this string text)
+        {
+            Task.Run(text.PromptError);
+        }
+
+        public static void NonBlockingPromptSuccess(this string text)
+        {
+            Task.Run(text.PromptSuccess);
+        }
     }
 }
